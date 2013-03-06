c0c8067a93704ff2f0fcab655ac918f188111e71
xwqgis <4508784@qq.com>
2013-03-06 15:00:45 +0800

    testtest

---
+++ demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by xwqgis on 13-3-6.
+//  Copyright (c) 2013年 xwqgis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
